ebb6bb03dd42a6491f6d19e96058344367f21c19
jyhong <jingyuan.hong@kcl.ac.uk>
2025-12-10 18:00:47 +0800

    Initial Commit

---
+++ det-evth Watch App/det_evthApp.swift
@@ -0,0 +1,17 @@
+//
+//  det_evthApp.swift
+//  det-evth Watch App
+//
+//  Created by jyhong on 10/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct det_evth_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
